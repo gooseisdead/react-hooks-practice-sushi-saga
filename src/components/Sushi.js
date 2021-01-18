d6b6ca4746f6cf76d940bbf57f769fb5a510c065
gooseisdead <gooseisdead@mac.com>
2021-01-18 16:36:02 -0500

    there we go...done

--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 function Sushi({ sushi, handleEating }) {
 
+  function clickHandler() {
+    if (!sushi.eaten) {
+      handleEating(sushi)
+    } else {
+      alert("Ask for more sushi.")
+    }
+  }
+
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={() => handleEating(sushi)}>
+           onClick={clickHandler}>
         { sushi.eaten ? null : <img src={sushi.img_url} width="100%" alt={sushi.name}/> }
       </div>
       <h4 className="sushi-details">
